15d23592bd38ae25629d9c1b0f0858d4ec843e0f
agasti-mhatre <agastimhatre6@gmail.com>
2024-08-22 18:00:35 -0400

    first commit

---
+++ src/main/java/recommender/recommenderBackend/listRetrieval/ListRetrievalApplication.java
@@ -0,0 +1,13 @@
+package recommender.recommenderBackend.listRetrieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListRetrievalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListRetrievalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/recommender/recommenderBackend/listRetrieval/ListRetrievalApplicationTests.java
@@ -0,0 +1,13 @@
+package recommender.recommenderBackend.listRetrieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListRetrievalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
